15198f5795b5bf22296623474d3ba21bffaa9847
Tyler Porter <tylerpporter@gmail.com>
2020-01-30 14:21:39 -0700

    Pass all event tests

--- test/event_test.rb
+++ test/event_test.rb
@@ -11,6 +11,29 @@ class EventTest < Minitest::Test
     assert_instance_of Event, @event
   end
 
-  
+  def test_it_stores_event_name
+    assert_equal "Curling", @event.name
+  end
+
+  def test_it_stores_age_data
+    assert_equal [24, 30, 18, 20, 41], @event.ages
+  end
+
+  def test_it_can_return_min_and_max_age
+    assert_equal 41, @event.max_age
+    assert_equal 18, @event.min_age
+  end
+
+  def test_it_can_return_the_average_age
+    assert_equal 26.6, @event.average_age
+  end
+
+  def test_it_can_return_standard_deviation
+    assert_equal 8.28, @event.standard_deviation_age
+  end 
+
+
+
+
 
 end
